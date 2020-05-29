66170b768b85674e412c3bc3d30a49a0936885f2
ankitRay1 <guser3288@gmail.com>
2020-05-29 17:35:13 +0530

    intial release

---
+++ application/views/admin/common/common_footer.php
@@ -0,0 +1,4 @@
+<footer class="main-footer non-print">
+         
+        <strong>Copyright &copy; 2020 <a href="https://ankitray1.github.io" target="_blank">Ankit</a>.</strong> All rights reserved.
+      </footer>
